a3235b6377d9875e2761c67d8eb2253ac6eb0e66
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-06-16 21:10:09 -0700

    add trainer page

--- src/components/trainer.js
+++ src/components/trainer.js
@@ -12,6 +12,18 @@ function Trainer() {
                 <span class="t_login100-form-title">
                     Trainers Page
                 </span>
+                <label class="select" for="slct">
+                <select id="slct" required="required">
+                    <option value="" disabled="disabled" selected="selected">Select option</option>
+                    <option value="#">One</option>
+                    <option value="#">Two</option>
+                    <option value="#">Three</option>
+                    <option value="#">Four</option>
+                    <option value="#">Five</option>
+                    <option value="#">Six</option>
+                    <option value="#">Seven</option>
+                </select>
+                </label>
                 <div class="t_wrap-input100 t_validate-input" data-validate = "Please enter password">
                     <input class="t_input100" type="password" name="pass" placeholder="Password"/>
                     <span class="t_focus-input100"></span>
